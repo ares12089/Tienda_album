a3730275dd5f9cf9af68fb7e70761dc7941462b2
ares <ares@ares>
2023-05-26 14:59:28 -0500

    main

---
+++ js/db_albums.js
@@ -0,0 +1,38 @@
+const albums = [
+    {
+        id: 1,
+        artista: "Los Diablitos",
+        pais: "Colombia",
+        img: "alb1.jpeg",
+    },
+
+    {
+        id: 2,
+        artista: "Los Chiches Vallenatos",
+        pais: "Colombia",
+        img: "alb2.jpeg"
+    },
+
+    {
+        id: 3,
+        artista: "Diomedez Diaz",
+        pais: "Colombia",
+        img: "alb3.jpeg"
+    },
+
+    {
+        id: 4,
+        artista: "Diomedez Diaz",
+        pais: "Colombia",
+        img: "alb4.jpeg"
+    },
+
+    {
+        id: 5,
+        artista: "Los Inquietos",
+        pais: "Colombia",
+        img: "alb5.jpeg"
+    },
+]
+
+var albumsCarrito = [];
